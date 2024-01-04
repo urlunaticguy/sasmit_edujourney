18fc4bde693083b60e459658d47f1515049acc02
Souvik Das <souvikvillain@icloud.com>
2024-01-04 19:30:58 +0530

    fixes

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 // index.js
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello, this is your backend app hosted on Vercel!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app; // Export the app for Vercel serverless functions
